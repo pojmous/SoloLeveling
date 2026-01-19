37bf7687663b7c8283221d5566b1510e92810bc7
pojmous <marekjozovic@gmail.com>
2026-01-19 19:43:33 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
 
-const CACHE_NAME = 'weekly-gate-pwa-v3-2-1';
+const CACHE_NAME = 'weekly-gate-pwa-v3-3-0';
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll([
-      './', './index.html', './app.js?v=321', './manifest.json', './icons/icon-192x192.png', './icons/icon-512x512.png'
-    ]))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll([
+    './', './index.html', './app.js?v=330', './manifest.json', './icons/icon-192x192.png', './icons/icon-512x512.png'
+  ])));
 });
 self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
